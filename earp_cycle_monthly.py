4f143ffc0a2086d10fa57cd63b82201c8a4a6589
Alex P. <proveaux@gmail.com>
2025-05-16 21:46:50 -0400

    updated cycle code

--- earp_cycle_monthly.py
+++ earp_cycle_monthly.py
@@ -3,7 +3,6 @@
 # simple script to clear copy interesting fields of a collaborative playlist to CSV, clear that playlist, then
 # copy all songs to a compliation playist.
 
-
 from spotipy.oauth2 import SpotifyOAuth
 from pprint import pprint
 import json
@@ -18,49 +17,64 @@ import discord_announce_v2
 
 # TODO handle yearly compilation playlist cycling
 # TODO build in args for debugging
+debug = 1
 
+if debug == 1:
+    print("We're Debugging")
+    ep_playlist_id = '2opAaOGzhp7txFUel5Qpic' #spotify playist ID "EP_Test"(Test)
+    ep_playlist_year = '0ctAvuxTyNOrC3BRjAfOqE' #spotify yearly playlist "EP_Year"(Test)
+    discord_bot_output_channel = 1309330887888080947 #discord (Test)
+else:
+    print("We're Live")
+    ep_playlist_id = '2HPyEPDBY7NZmOV72s5rie' #spotify playist ID "Ear Porn!!(Live)"
+    ep_playlist_year = '1WLV70aRmdxZbGXO9EG4oU' #spotify yearly playlist "EP_2025Collective"(Live)
+    discord_bot_output_channel = 780292448298467333 #discord (Live)
+
+sfquery = SpotipyAuthJson()
 date = str(datetime.datetime.now().strftime('%Y_%m')) #var for dateyime in YYYY_MM_DD formay
 year = str(datetime.datetime.now().strftime('%Y'))
-sfquery = SpotipyAuthJson()
-ep_playlist_id_debug = '2opAaOGzhp7txFUel5Qpic' #spotify playist ID "EP_Test"(Test)
-ep_playlist_id = '2HPyEPDBY7NZmOV72s5rie' #spotify playist ID "Ear Porn!!(Live)"
+
 ep_playlist_month = sfquery.sp.playlist(ep_playlist_id) #imports playlist as python dict
-ep_playlist_year_debug = '0ctAvuxTyNOrC3BRjAfOqE' #spotify yearly playlist "EP_Year"(Test)
-ep_playlist_year = '1WLV70aRmdxZbGXO9EG4oU' #spotify yearly playlist "EP_2025Collective"(Live)
 playlist_name = ep_playlist_month['name'] # var for playlist name
+
+track_id_month = [] # store track-IDs for this cycle
+
+discord_bot=discord_announce_v2.DiscordBot()
+
+# File creation operations go here
 app_dir=os.path.join(Path.home(), 'spotify_cycle')
 output_dir=os.path.join(Path.home(), app_dir, "outputs")
-file_yearly_pl_csv = open((Path(output_dir)) / Path(playlist_name + '_' + date + '.csv'), 'a')
-track_id_month = [] # store track-IDs for this cycle
-debug = 0
 
+try:
+    os.makedirs(output_dir)
+    print('Creating output directory')
+except FileExistsError:
+    print('Output Folder already exists.')
+
+file_yearly_pl_csv = open((Path(output_dir)) / Path(playlist_name + '_' + date + '.csv'), 'a')
+
+
+def discordAnnouncer(text):
+    for part in split_multiline_string(text):
+        print(part)
+        discord_bot.send(discord_bot_output_channel, part)#debug discord
+        #discord_bot.send(780292448298467333, part)#real discord
 
 def cycle():
-    discord_song_output="Last Month's Recap\n------------------\n"
-    #
-    trackdata = [] 
-   
-    home_dir=Path.home()
-    script_dir=os.path.dirname(os.path.abspath(__file__))
-    #output_dir=home_dir / "spotify_cycle" / "outputs"
-    discord_bot=discord_announce_v2.DiscordBot()
-    print(type(ep_playlist_month))
+    discord_song_output="Last Month's Recap\n------------------\n" # Init the discord output with a header
 
-    # File creation operations go here
-    try:
-        os.makedirs(output_dir)
-        print('Creating output directory')
-    except FileExistsError:
-        print('Output Folder already exists.')
-
+    trackdata = [] # Init trackdata list
+    
     file_monthly_pl_json = open((Path(output_dir)) / Path(playlist_name + '_' + date + '.json'), 'w')
+    file_monthly_pl_csv = open((Path(output_dir)) / Path(playlist_name + '_' + date + '.csv'), 'a')
     file_yearly_pl_csv = open((Path(output_dir)) / Path(playlist_name + '_' + year + '.csv'), 'a')
+
     print(json.dumps(ep_playlist_month, indent=4) ,file=file_monthly_pl_json) #
     #discord_bot.send(1309330887888080947, 10*"YEET!\n")
 
-    #TODO change json_to_csv_fields name to LIST
+    # TODO change json_to_csv_fields name to LIST
     # TODO handle choosing interesting fields better.
-    #Loop through all tracks in the playlist, write interesting fields to csv
+    # Loop through all tracks in the playlist, write interesting fields to csv
     for track in ep_playlist_month.get("tracks", {}).get("items", []):
         if track.get("track") is None:
             print("Found a track with value None")
@@ -73,74 +87,62 @@ def cycle():
                                 track["added_at"],
                                 track["track"]["id"],
                                 ]
-        # print("test")
-        # print(json_to_csv_fields)
         
         trackdata.append(json_to_csv_fields) #append song entry data list to trackdata dictionary
         track_id_month.append(json_to_csv_fields[-1]) #append song ID to track_id_month list for later use in moving songs between playlist
         #song data to print to discord
-        # discord_song_output += track["added_by"]["id"] + " - " +track["track"]["name"] + " by " + track["track"]["album"]["artists"][0]["name"] + " at " + track["added_at"] + "\n"
-        # discord_song_output += track["added_by"]["id"] + " - " +track["track"]["name"] + " by " + track["track"]["album"]["artists"][0]["name"]+"\n"
         discord_song_output += usernameFixer(track["added_by"]["id"]) + " - " +track["track"]["name"] + " by " + str(track["track"]["album"]["artists"][0]["name"])+"\n"
-        #print(f"{str(track["added_by"]["id"])} added {str(track["track"]["name"])} by {str(track["track"]["album"]["artists"][0]["name"])} at {str(track["added_at"])}")
     
-
     #CSV Writer
-    #TODO - this needs work. It should add headers on init.
-    #pprint(type(trackdata))
-    trackdata_headers = ["Track","Album","Artist","Added By","Time Added","Track ID"]
-    with open(file_yearly_pl_csv.name, 'a',newline="") as f:
-        writer = csv.writer(f, delimiter=',')
-        write_headers = not os.path.exists(file_yearly_pl_csv.name) or os.path.getsize(file_yearly_pl_csv.name) == 0
-    
-    # Write headers if the file is new or empty
-        if write_headers:
-            writer.writerow(trackdata_headers)
+    def trackwriter(filename):
+        trackdata_headers = ["Track","Album","Artist","Added By","Time Added","Track ID"]
+        with open(filename.name, 'a',newline="") as f:
+            writer = csv.writer(f, delimiter=',')
+            write_headers = not os.path.exists(filename.name) or os.path.getsize(filename.name) == 0
+        
+        # Write headers if the file is new or empty
+            if write_headers:
+                print("Writing header to new file")
+                writer.writerow(trackdata_headers)
 
-        writer.writerows(trackdata)
+            print("Writing trackdata to " + str(filename)) 
+            writer.writerows(trackdata)
+    
+    trackwriter(file_yearly_pl_csv)
+    trackwriter(file_monthly_pl_csv)
 
     # Playlist manipulation logic starts here
-    # clear contents of this month's track IDs from the monthly playlist ##################
+    # Clear contents of this month's track IDs from the monthly playlist ##################
     sfquery.sp.playlist_remove_all_occurrences_of_items(ep_playlist_id, track_id_month) 
-    # write contents of this months track IDs to the yearly playlist
+    # Write contents of this months track IDs to the yearly playlist
     sfquery.sp.playlist_add_items(ep_playlist_year, track_id_month)
-    #print(f"{str(track["added_by"]["id"])} added {str(track["track"]["name"])} by {str(track["track"]["album"]["artists"][0]["name"])} at {str(track["added_at"])}")
 
     # Playlist manipulation logic ends here
     if debug == 1:
         debugCycle()
 
-    # print(discord_song_output)
-
-    print(type(split_multiline_string(discord_song_output)))
-    for part in split_multiline_string(discord_song_output):
-        print(part)
-        # discord_bot.send(1309330887888080947, part)#debug discord
-        discord_bot.send(780292448298467333, part)#real discord
-       
-    # discord_bot.send(1309330887888080947, discord_song_output)#print song data to discord Debug.
-    #discord_bot.send(780292448298467333, discord_song_output)#print song data to discord.
+    # Announce all songs contributed by all users when cycled
+    discordAnnouncer(discord_song_output)
 
     return(discord_song_output)
 
 def debugCycle():
     #Adds a block of songs to the test monthly playlist for debugging.
-    if debug == 1:
-        ep_playlist_id = '2HPyEPDBY7NZmOV72s5rie' #spotify playist ID "EP_Test"
-        track_id_month = ['6ie0uyyvOKTTuIFBMPiNIl', 
-                        '0C9u106kRYCqYSP3KDdk3v', 
-                        '7jBAskQhyfjmbYC0o3pXdW', 
-                        '1Jg3XdrCOZ5rrirIOggdtW', 
-                        '6dU5RxthbuaN31bRbEDlNw', 
-                        '0ZK8TGOsngrstVPsnrHbK1', 
-                        '0iCrjwLMTjWsdOKdOAZ0FC', 
-                        '3uC4r2daXertBxxc8BpbbN', 
-                        '21qnJAMtzC6S5SESuqQLEK', 
-                        '4efAv86uRxR4yQBcb3Vczq',
-                        '6LQAeEZ1zbZUZ5ItQI5l1b',
-                        ]
-        time.sleep(5)
-        sfquery.sp.playlist_add_items(ep_playlist_id, track_id_month)
+    print("Resetting debugging playlist")
+    track_id_month = ['6ie0uyyvOKTTuIFBMPiNIl', 
+                    '0C9u106kRYCqYSP3KDdk3v', 
+                    '7jBAskQhyfjmbYC0o3pXdW', 
+                    '1Jg3XdrCOZ5rrirIOggdtW', 
+                    '6dU5RxthbuaN31bRbEDlNw', 
+                    '0ZK8TGOsngrstVPsnrHbK1', 
+                    '0iCrjwLMTjWsdOKdOAZ0FC', 
+                    '3uC4r2daXertBxxc8BpbbN', 
+                    '21qnJAMtzC6S5SESuqQLEK', 
+                    '4efAv86uRxR4yQBcb3Vczq',
+                    '6LQAeEZ1zbZUZ5ItQI5l1b',
+                    ]
+    time.sleep(5)
+    sfquery.sp.playlist_add_items(ep_playlist_id, track_id_month)
 
 def addBubbleButt():
     sfquery.sp.playlist_add_items(ep_playlist_id, ['6LQAeEZ1zbZUZ5ItQI5l1b'])
@@ -208,7 +210,6 @@ def split_multiline_string(input_string, max_length=2000):
     
     return parts
 
-#TODO account for singular vs plural
 def listContributers():
     ep_playlist_month = sfquery.sp.playlist(ep_playlist_id) #imports playlist as python dict
     userSongCount = {}
@@ -223,27 +224,16 @@ def listContributers():
     for user in userSongCount: 
         if len(user) > un_length:
             un_length = len(user) + 1
-    
-    #print(userSongCount)
-    # output= 'User'.center(un_length) + "Count" +'\n'
-
-    # # for user in userSongCount:
-    # #     # print(len(user)) 
-    # #     # print(user)
-    # #     output += user + '-' * (un_length - len(user)) + str(userSongCount[user]) + '\n'
         
     #original output style
     for user in userSongCount:
         output += user + ' contributed ' + str(userSongCount[user]) + " songs" + "\n"
     return (output)
 
-
-
 def main():
     cycle()
     #print(usernameFixer('Alex'))
     #print(listContributers())
-
 
 if __name__ == '__main__':
     main()
