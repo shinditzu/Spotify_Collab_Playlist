f678dc7b0e85879cc9997a72579778b60433ea7a
shinditzu <58868660+shinditzu@users.noreply.github.com>
2025-08-19 22:22:41 -0400

    normal schedule interval

--- spotify_cycle_service.py
+++ spotify_cycle_service.py
@@ -35,10 +35,10 @@ def job():
         print(f"Today is {today} time is {now.strftime('%H:%M')}, not the last day of the month. Running debug flow.")
         cycle(use_debug=True)
 
-#schedule.every(1).day.at("00:05").do(job)
-schedule.every(1).minute.do(job)
+schedule.every(1).day.at("00:05").do(job)
+#schedule.every(1).minute.do(job)
 
 while True:
     print("Checking if it's time to run the job...")
     schedule.run_pending() #check for jobs to run.
-    time.sleep(30) #sleep for 30 seconds
+    time.sleep(60) #sleep for 60 seconds
