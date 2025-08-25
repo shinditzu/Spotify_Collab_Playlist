148e2cf80b91b4ac2e52c930614efea09d810068
shinditzu <58868660+shinditzu@users.noreply.github.com>
2025-08-25 01:04:09 -0400

    Fixed a bug

--- bot_tools.py
+++ bot_tools.py
@@ -2,40 +2,20 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import os
 from pprint import pprint
-
+import random
 
 load_dotenv()
-#print(os.getenv("OPENAI_API_KEY"))
-
-# yearly_user_song_data = parse_yearly_data_by_user(use_debug=)
-# yearly_data_str = str(yearly_user_song_data)
-
-#pprint(yearly_user_song_data)
-
-# song_data_for_prompt = []
-
-# for i, user in enumerate(yearly_user_song_data):
-#     prompt = ''
-
-#     prompt = f'''
-# User: {user}
-# Songs Contributed: {len(yearly_user_song_data[user])}\n'''
-    
-#     for song in yearly_user_song_data[user]:
-#         prompt += f" - {song['Track']} by {song['Artist']}\n"
-#     print(prompt)
-#     song_data_for_prompt.append(prompt)
-    
-#pprint(song_data_for_prompt)
-
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-tone_list = ["fun", "poetic", "straightforward", "snarky"]
+tone_list = ["fun", "poetic", "straightforward", "mean", "snarky"]
 rhyme = False
+random_tone = random.choice(tone_list)
+
 response_content = []
 
-def ai_monthly_commentary(song_data, tone=tone_list[2]):
+
+def ai_monthly_commentary(song_data, tone="straightforward"):
     for user in song_data:
         songs = "\n".join('Track: ' + song['Track'] + ' Artist: ' + song['Artist'] for song in song_data[user]) + "\n"
         #print(songs)
@@ -45,12 +25,12 @@ def ai_monthly_commentary(song_data, tone=tone_list[2]):
                 {
                     "role": "system",
                     "content": (
-                        "You are a music critic. Write a commentary about a user's musical taste."
+                        f"You are a music critic. Write a commentary about a user's musical taste."
 
                         f"""INSTRUCTIONS:
                         - Style: {tone}.
                         - Output EXACTLY three sentences.
-                        - Be specific: reference patterns (e.g., novelty, internet humor, classic rock, soulful/nostalgic picks).
+                        - Be specific: reference patterns (e.g., novelty, internet humor, classic rock, soulful/nostalgic picks, etc).
                         - Avoid emojis. Keep it warm, tight, and readable."""
 
                         "USER DATA:"
